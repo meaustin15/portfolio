37248c6f287629ef008189dee5624f64210b5901
maustin2015 <yebk4lyfe@gmail.com>
2017-07-12 10:05:19 -0700

    project change

--- scripts.js
+++ scripts.js
@@ -2,3 +2,13 @@ function raiseCurtain() {
     var curtain = document.getElementsByClassName("curtain");
     curtain[0].classList.add("raise");
 }
+
+function submitContactForm(){
+    var x = document.forms["contactForm"]["email"].value;
+    if (x== ""){
+        alert("Email must be filled out");
+        return false;
+    } else {
+        alert("Thank you for submitting the form");
+    }
+}
